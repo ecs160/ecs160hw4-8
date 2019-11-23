2969474adafabce9e6acd78afe51a97157950e0f
Haoston Huynh <haostonh@Haostons-MacBook-Pro.local>
2019-11-23 15:28:20 -0800

    Added another csv with around 10 tweeters to check they are printing out

--- maxTweeter.c
+++ maxTweeter.c
@@ -5,23 +5,28 @@
 int main(int argc, char** argv){
     FILE *csv_text;
     char str[100];
+    char *eachline = NULL;
+    size_t len = 1000;
+    ssize_t line_size;
 
     if (argc != 2){
         printf("Not Valid Input\n");
-        return 0;
+        exit(0);
     }
 
     if (argc == 2){
-        csv_text = fopen(argv[0], "r");
-        char *column_text = strtok("hi", ",");
-        printf(column_text);
-        while (fgets(str, 100, csv_text) != NULL)
-            printf("%s", str);
-        fclose(csv_text);
+        csv_text = fopen(argv[1], "r");
+        if (csv_text == NULL) {
+            printf("Can't open file\n");
+            exit(0);
+        }
+        while (getline(&eachline, &len, csv_text) != -1){
+            printf("%s\n",eachline);
+        }
     }
-    printf("hello\n");
+    
 
     return 0;
 }
 
-//"cl-tweets-short-clean.csv
+//"cl-tweets-short-clean.csv"
